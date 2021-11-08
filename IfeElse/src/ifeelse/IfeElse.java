f88a9639d6c592d04069f6ffd08da1a185ee19cf
Administrador <Administrador@CAM192982>
2021-11-08 20:46:10 -0300

    IfeElse

---
+++ IfeElse/src/ifeelse/IfeElse.java
@@ -0,0 +1,22 @@
+package ifeelse;
+
+public class IfeElse {
+    
+    public static void main(String[] args) {
+    boolean var1 = true;
+    //Se var1 for verdadeiro
+    if (var1){
+        System.out.println("var1: Verdadeiro");
+    }else{
+        //opção falsa
+        System.out.println("var1: Falso");
+    }
+    //Se var1 for falso
+   if(!var1){
+       System.out.println("var1: Verdadeiro");
+    }else{
+       System.out.println("var1: Falso");
+   }
+    }
+    
+}
