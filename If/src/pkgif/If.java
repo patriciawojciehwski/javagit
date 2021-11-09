f306485ac1973be516bd0408247a6fa57e4ef683
Administrador <Administrador@CAM192982>
2021-11-08 21:06:13 -0300

    If

---
+++ If/src/pkgif/If.java
@@ -0,0 +1,28 @@
+
+package pkgif;
+
+public class If {
+    
+    public static void main(String[] args) {
+        int idade= 85;
+        if (idade <= 40){
+            System.out.println("Menor que 40");
+       }
+        else if (idade<=60){
+            System.out.println("Menor que 60");
+        }
+        else if (idade <=80){
+            System.out.println("Menor que 80");
+        }
+        else if (idade >=101){
+            System.out.println("Menor que 100");
+        }
+        else{
+            System.out.println("Invalido");
+        }
+           }    
+        }
+            
+    
+    
+
