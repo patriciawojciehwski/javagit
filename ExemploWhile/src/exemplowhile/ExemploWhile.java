338974368d7ac153fd5876f884cf9268dd10dd9f
Administrador <Administrador@CAM192982>
2021-11-10 21:27:25 -0300

    ExemploWhile

---
+++ ExemploWhile/src/exemplowhile/ExemploWhile.java
@@ -0,0 +1,15 @@
+
+package exemplowhile;
+
+public class ExemploWhile {
+   
+    public static void main(String[] args) {
+     
+        int contador = 0;
+        while (contador<50){
+            System.out.println("Repetição numero: " + contador);
+            contador++;
+        }
+    }
+    
+}
