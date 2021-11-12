629e90ab5ee6f6b3b3027ccb8116f0ba81e56e15
Administrador <Administrador@CAM192982>
2021-11-11 21:22:18 -0300

    Cad

---
+++ Cad/src/cad/Cad.java
@@ -0,0 +1,15 @@
+
+package cad;
+
+public class Cad {
+   
+    public static void main(String[] args) {
+    
+        int numeros = 0;
+        for (numeros=1; numeros<=10; numeros++)
+        {
+            System.out.println("Tabuada: 1 x " + numeros + " = " + 1*numeros);
+        }    
+    }
+    
+}
