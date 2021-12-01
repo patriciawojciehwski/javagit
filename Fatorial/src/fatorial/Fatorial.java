1b96054964dc849e9975f492b0274a1cf4b62c18
Administrador <Administrador@CAM192982>
2021-12-01 20:31:48 -0300

    Cad

---
+++ Fatorial/src/fatorial/Fatorial.java
@@ -0,0 +1,14 @@
+
+package fatorial;
+
+public class Fatorial {
+ 
+    public static void main(String[] args) {
+      
+        int fator, resultado = 1;
+        for(fator=5; fator>1; fator--)
+            resultado*= fator;
+        System.out.println("resultado");
+    }
+    
+}
